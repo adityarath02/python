0ddb3ebda0ecf1b4f159efe091c324f1c30f27e0
Aditya Rath <135094190+adityarath02@users.noreply.github.com>
2024-09-06 13:39:40 +0530

    Add files via upload

---
+++ areaofarect.py
@@ -0,0 +1,9 @@
+length = input ("What is the length?")
+breadth = input ("What is the breadth?")
+
+length = float(length)
+breadth = float(breadth)
+
+area = length * breadth
+
+print(f"The area is {area}")
---
+++ shoppingcart.py
@@ -0,0 +1,12 @@
+
+
+item = input("What do you want?")
+price = float(input("What is the price of the item?" ""))
+quantity = int(input("How many do you want?" ""))
+total = price * quantity
+
+print(f"You Ordered : {item}")
+print(f"Price of the Item : {price}")
+print(f"Quantity : {quantity}")
+
+print(f"Your total is {total}")
